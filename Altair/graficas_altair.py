2d3fef94374b150314ff3a209a42e7ddd4184640
July230 <A01352823@tec.mx>
2025-11-18 16:28:58 -0600

    Importar un df de seaborn y mostrar tabla

--- Altair/graficas_altair.py
+++ Altair/graficas_altair.py
@@ -2,5 +2,12 @@
 
 import streamlit as st
 import altair as alt
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset('penguins')
 
 st.title('Gráficas con Altair')
+st.write('Este es el primer párrafo de mi aplicativo web con streamlit')
+st.write('Este es el segundo párrafo de mi aplicativo web con streamlit')
+st.table(df.sample(10))
