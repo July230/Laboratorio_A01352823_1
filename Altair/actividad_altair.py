f51d2a9c1e6f36273c104f8f66e4312bbaf458e6
July230 <A01352823@tec.mx>
2025-11-18 18:15:56 -0600

    Agregar gráficas

--- Altair/actividad_altair.py
+++ Altair/actividad_altair.py
@@ -35,5 +35,61 @@ graf1 = alt.Chart(df).mark_point().encode(
     width=400,
     height=400
 ).interactive()
+st.altair_chart(graf1)
+st.write('''El gráfico de dispersión la puntuación de felicidad contra el Producto Interno Bruto (PIB) de ada país. 
+         La gama de colores seleccionada fue la gama triada a partir del color naranja de la empresa FedEx, utilizada en este caso para colorear los países por región.
+         Aquí podemos ver que la mayoría de países de África subsahariana están por debajo de los 5.5 puntos de felicidad. 
+         Esto destaca la importancia de las visualizaciones, puesto que la anterior gráfica de barras era una suma total de la región.''')
 
-graf1
+df = df.sort_values(by='Happiness Rank')
+paises_mas_felices = df.head(10)
+orden = list(paises_mas_felices.sort_values('Happiness Score')['Country'])
+
+graf4 = alt.Chart(paises_mas_felices).mark_bar().encode(
+    x=alt.X('Country:N', sort='y'), # ordenar de menor a mayor
+    y='Happiness Score:Q',
+    color=alt.Color('Country:N',
+                    sort=orden,
+                    scale=alt.Scale(
+                        domain=orden,
+                        range=gama_triada_naranja
+                    )),
+    tooltip=['Happiness Score'],
+).properties(
+    title='Países más felices',
+    width=400,
+    height=400
+)
+st.altair_chart(graf4)
+st.write('''El siguiente gráfico de barras muestra la puntuación de felicidad de los 10 países más felices.
+            En esta ocasión están ordenados de forma ascendente siguiendo una buena práctica para los gráficos de barras. 
+            La gama de color es la misma, siguiendo el mismo patrón ascendente.''')
+
+intervalo = alt.selection_interval()
+graf7 = alt.Chart(df).mark_point().encode(
+    x='Health (Life Expectancy):Q',
+    y='Economy (GDP per Capita):Q',
+    color=alt.Color('Region:N',
+                    scale=alt.Scale(range=gama_triada_naranja)),
+    tooltip=['Country']
+).properties(
+    title='Espectativa de vida vs PIB',
+    width=300,
+    height=300
+).add_params(intervalo)
+
+graf8 = alt.Chart(df).mark_point().encode(
+    x='Health (Life Expectancy):Q',
+    y='Economy (GDP per Capita):Q',
+    color=alt.Color('Region:N',
+                    scale=alt.Scale(range=gama_triada_naranja)),
+    tooltip=['Country']
+).properties(
+    title='Espectativa de vida vs PIB',
+    width=300,
+    height=300
+).transform_filter(intervalo)
+st.altair_chart(graf7 | graf8)
+st.write('''El gráfico de dispersión muestra la expectativa de vida con respecto al PIB. 
+         En este caso, sí parece haber una correlación positiva, donde entre más alto sea el PIB, mayor esperanza de vida. 
+         Por ejemplo, los paises correspondientes a Europa occidental se concentran en un área del gráfico.''')
