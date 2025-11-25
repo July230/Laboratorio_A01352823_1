130ff5ab4ec52f662c749b7f42af0cc5b8eba729
July230 <A01352823@tec.mx>
2025-11-25 16:41:32 -0600

    Codificar inputs

--- Altair/penguins_dashboard.py
+++ Altair/penguins_dashboard.py
@@ -25,6 +25,21 @@ with st.form('user_input'):
     sex = st.selectbox('Sex', options=['Male', 'Female'])
     bill_length_mm = st.number_input('Bill lenght (mm)', min_value=0)
     bill_depth_mm = st.number_input('Bill depth (mm)', min_value=0)
-    flipper_lenght_mm = st.number_input('Flipper length (mm)', min_value=0)
+    flipper_length_mm = st.number_input('Flipper length (mm)', min_value=0)
     body_mass_g = st.number_input('Body mass (g)', min_value=0)
     st.form_submit_button()
+
+# codificar inputs
+island_torgersen, island_biscoe, island_dream = 0, 0, 0
+if island == 'Torgersen':
+    island_torgersen = 1
+if island == 'Biscoe':
+    island_biscoe = 1
+if island == 'Dream':
+    island_dream = 1
+
+sex_female, sex_male = 0, 0
+if sex == 'Female':
+    sex_female = 1
+if sex == 'Male':
+    sex_male = 1
