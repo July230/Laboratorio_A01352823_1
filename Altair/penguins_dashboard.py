f50794514d10e17c1c78ac77a7707af08a4e2da8
July230 <A01352823@tec.mx>
2025-11-25 16:15:32 -0600

    Leer archivos pickle

--- Altair/penguins_dashboard.py
+++ Altair/penguins_dashboard.py
@@ -10,3 +10,12 @@ from sklearn.model_selection import train_test_split
 st.set_page_config(layout='wide')
 st.title('Demo del modelo para penguins')
 st.write('Esta app nos permite generar predicciones de especies de pinguinos')
+
+# Procedemos a cargar el modelo guardado como pickle
+with open('random_forest_classifier.pickle', 'rb') as rf_pickle:
+    rfc = pickle.load(rf_pickle)
+    rf_pickle.close()
+
+with open('output_penguins.pickle', 'rb') as map_pickle:
+    unique_penguin_mapping = pickle.load(map_pickle)
+    map_pickle.close()
